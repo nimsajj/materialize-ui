ba7708811e133bf1d4cd9acae2210a2c3d37cb0c
nimsajj <nimsaj.dev@gmail.com>
2020-11-21 18:48:57 +0100

    create and configure storybook

---
+++ packages/materialize-ui-carousel/docs/materialize-ui-carousel.stories.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import Carousel from "../lib/materialize-ui-carousel";
+
+export default {
+  title: "Carousel",
+};
+
+export const Default = () => <Carousel />;
---
+++ packages/materialize-ui-dropdown/docs/materialize-ui-dropdown.stories.js
@@ -0,0 +1,6 @@
+import React from "react";
+import Dropdown from "../lib/materialize-ui-dropdown";
+
+export default { title: "Dropdown", component: Dropdown };
+
+export const Default = () => <Dropdown />;
